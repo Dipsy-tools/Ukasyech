2f494c77add852b7c3d02b0a29e8507fbf417e3b
DeXploit404 <rterangel14@gmail.com>
2024-12-01 19:56:54 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,6 @@
+// Example: Add animations or click effects
+document.querySelectorAll('.link-button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Redirecting...');
+    });
+});
